58cef13eb12cd06103e9637ecde98eeff72c37fb
Marcelo Klinger <marcelo.klinger@hotmail.com>
2019-07-15 19:59:16 -0300

    Projeto Instalura

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css';
+import App from './App';
+import Login from './componentes/Login';
+import {Router, Route, browserHistory} from 'react-router';
+
+
+ReactDOM.render(
+  <Router history={browserHistory} >
+    <Route path="/" component={Login}/>
+    <Route path="/timeline" component={App}/>
+  </Router>,
+  document.getElementById('root')
+);
